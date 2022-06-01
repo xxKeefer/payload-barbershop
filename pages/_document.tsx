c9b6f5913660e192baa3f3a72086f91245159f08
xxKeefer <xxkeefer.code@gmail.com>
2022-06-01 20:33:42 +1000

    feat(config): :wrench: update eslint, prettier, tsconfig and _document

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head>
+                <meta
+                    httpEquiv="Content-Type"
+                    content="text/html; charset=utf-8"
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
